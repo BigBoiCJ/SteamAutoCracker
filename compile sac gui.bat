76ea52b0b3d415dfb668643d2501e3f521c4a599
BigBoiCJ <101492671+BigBoiCJ@users.noreply.github.com>
2024-04-27 17:40:06 +0200

    Added sac gui compile script

---
+++ compile sac gui.bat
@@ -0,0 +1,8 @@
+call .venv\Scripts\activate.bat
+pyinstaller --onefile --windowed --icon=icon_hashtag.ico sac_files_here\steam_auto_cracker_gui.py
+timeout /t 2 /nobreak > NUL
+rmdir /S /Q build
+del steam_auto_cracker_gui.spec
+move dist\steam_auto_cracker_gui.exe "%cd%"
+rmdir dist
+pause

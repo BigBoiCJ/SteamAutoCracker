e2d0c93d54f550b55fe9b67a85c70250ec651bcb
BigBoiCJ <101492671+BigBoiCJ@users.noreply.github.com>
2024-04-28 19:03:12 +0200

    Delete sac_lib directory

--- sac_lib/get_file_version.py
+++
@@ -1,5 +0,0 @@
-import win32api
-
-def GetFileVersion(filename: str) -> str:
-    fileInfos = win32api.GetFileVersionInfo(filename, "\\")
-    return "%d.%d.%d.%d" % (fileInfos['FileVersionMS'] / 65536, fileInfos['FileVersionMS'] % 65536, fileInfos['FileVersionLS'] / 65536, fileInfos['FileVersionLS'] % 65536)

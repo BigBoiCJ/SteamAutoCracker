7f6dcfac042e74b64e77227c69c74ee92cb0efe0
BigBoiCJ <101492671+BigBoiCJ@users.noreply.github.com>
2024-08-14 00:01:07 +0200

    Added pyinstaller hooks dir

--- compile sac gui.bat
+++ compile sac gui.bat
@@ -1,5 +1,5 @@
 call .venv\Scripts\activate.bat
-pyinstaller --onefile --windowed --icon=icon_hashtag.ico sac_files_here\steam_auto_cracker_gui.py
+pyinstaller --onefile --windowed --icon=icon_hashtag.ico sac_files_here\steam_auto_cracker_gui.py --additional-hooks-dir=./hooks/
 timeout /t 2 /nobreak > NUL
 rmdir /S /Q build
 del steam_auto_cracker_gui.spec
